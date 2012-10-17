dd12141b645f5436480349dec71e457f1fbd7010
Willem van der Schyff <willemvds@gmail.com>
2012-10-17 20:42:34 +0200

    STEVE!

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func Print(cv *ChatView) {
+	fmt.Println("Received from", cv.GetRemote(), ":", cv.GetText())
+}
+
+func Log(cv *ChatView) {
+	log.Print("Received from", cv.GetRemote(), ":", cv.GetText())
+}
+
+func Reply(cv *ChatView) {
+	XMPPSendMessage(cv.GetRemote(), fmt.Sprintf("ACK: %s", cv.GetText()))
+}
+
+func main() {
+	user := flag.String("user", "", "gtalk username")
+	passwd := flag.String("passwd", "", "gtalk password")
+	flag.Parse()
+
+	if len(*user) == 0 || len(*passwd) == 0 {
+		flag.PrintDefaults()
+		return
+	}
+	StartXMPP("talk.google.com:443", *user, *passwd)
+
+	AddXMPPHandler(Print)
+	AddXMPPHandler(Log)
+	AddXMPPHandler(Reply)
+
+	for {
+		in := bufio.NewReader(os.Stdin)
+		line, err := in.ReadString('\n')
+		if err != nil {
+			continue
+		}
+		line = strings.TrimRight(line, "\n")
+		tokens := strings.SplitN(line, " ", 2)
+		if len(tokens) == 2 {
+			err := XMPPSendMessage(tokens[0], tokens[1])
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+
+	select {}
+}
